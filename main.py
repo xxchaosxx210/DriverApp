2c5d8546d883090b7f704da634570f09ab0f6199
xxchaosxx210 <pmillar1279@googlemail.com>
2021-01-28 17:53:38 +0000

    added bottom navigation bar

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+__version__ = "0.1"
+__author__ = "Paul Millar"
+__description__ = "Driver App prototype"
+
 from kivymd.app import MDApp
 
 class MainApp(MDApp):
@@ -5,10 +9,7 @@ class MainApp(MDApp):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         # tell kivymd what you theme you want for the App
-        self.theme_cls.primary_palette = "Teal"
-    
-    def on_start(self):
-        print(f"{self.root.current}")
+        self.theme_cls.primary_palette = "Blue"
         
 def main():
     MainApp().run()
