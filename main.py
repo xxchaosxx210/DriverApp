73f965fef336fbec35b15587feaee59bc83ce017
xxchaosxx210 <pmillar1279@googlemail.com>
2021-01-28 21:44:15 +0000

    fixed the bottomnavigation not updating

--- main.py
+++ main.py
@@ -2,7 +2,27 @@ __version__ = "0.1"
 __author__ = "Paul Millar"
 __description__ = "Driver App prototype"
 
+
 from kivymd.app import MDApp
+from kivymd.uix.screen import MDScreen
+from kivy.properties import StringProperty
+from kivy.uix.screenmanager import ScreenManager
+
+
+class AppScreenManager(ScreenManager):
+    pass
+
+
+class LoginMDscreen(MDScreen):
+    
+    username = StringProperty('')
+    password = StringProperty('')
+    
+    def on_login(self):
+        print(self.username)
+        print(self.password)
+        self.manager.current = "main"
+    
 
 class MainApp(MDApp):
     
